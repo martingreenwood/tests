c7df68238d2f027207191d23326eaf66190479ef
1 <martin@pixelpudu.com>
2018-02-01 13:13:47 +0000

    initial

---
+++ huw.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HUW</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body class="huw">
+
+	<header id="header" class="">
+
+		<div class="container">
+
+			<div class="row">
+				<h1>HUW Tests</h1>
+			</div>
+		
+		</div>
+		
+	</header><!-- /header -->
+	<section class="container">
+
+		<div class="row">
+
+		</div>
+		
+	</section>
+
+</body>
+</html>
